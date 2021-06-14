f396156901f0c66a9661edd12ec2691e1df05d3b
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-06-13 22:16:15 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DataBindingNotesComponent } from './data-binding-notes/data-binding-notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DataBindingNotesComponent
   ],
   imports: [
     BrowserModule,
