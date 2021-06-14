590c1fe3e85ddc77e9f82f5c2ad190c5733996ad
augustoibarrola <augusto.t.ibarrola@gmail.com>
2021-06-13 22:22:59 -0400

    deleted unneeded component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { DataBindingNotesComponent } from './data-binding-notes/data-binding-notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    DataBindingNotesComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
